676d08faf28e78c2c27e17351e652d19d3a023ff
Adbhut1234 <adbhutpandey12@outlook.com>
2026-06-30 20:31:59 +0530

    pc control setup

--- computer_use.py
+++ computer_use.py
@@ -2,7 +2,9 @@ import os, io, json, time, base64, logging
 import pyautogui
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
 
+load_dotenv()
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ACTION_SCHEMA = """
